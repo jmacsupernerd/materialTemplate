b996e579ee2cfaccbadc681459f5454fc30c34ff
joshuamcwilliams <jtmacnb@gmail.com>
2015-01-06 21:33:03 -0600

    first commit

---
+++ app/src/main/java/com/mcwilliams/template/Home.java
@@ -0,0 +1,13 @@
+package com.mcwilliams.template;
+
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by joshuamcwilliams on 1/6/15.
+ */
+public class Home extends MainActivity {
+
+
+}
